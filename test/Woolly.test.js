d70e0606d0bbda853e998fa04f1bc9890d67abf8
allain <allain.lalonde@favequest.com>
2017-05-04 20:56:40 -0400

    using a better diff/patch algorithm making action results get returned to client

--- test/Woolly.test.js
+++ test/Woolly.test.js
@@ -1,8 +1,23 @@
 const test = require('tape')
+
 test.onFinish(() => process.exit()) // takes a while for the server to tear itself down
 
 const WoollyServer = require('..').WoollyServer
 const WoollyClient = require('..').WoollyClient
+
+const tearDown = (client, server, cb) => {
+  client.disconnect(
+    err => (err ? cb(err) : setTimeout(() => server.close(cb), 50))
+  )
+}
+
+const buildCountingHandler = (count = 0) => ({
+  route: '/count',
+  getState: () => count,
+  actions: {
+    inc: () => ++count
+  }
+})
 
 test('Woolly - can be installed onto a server', t => {
   const app = require('express')()
@@ -11,6 +26,7 @@ test('Woolly - can be installed onto a server', t => {
 
   t.equal(typeof w, 'object', 'factory builds objects')
   t.equal(typeof w.handler, 'function', 'should have handle method')
+
   server.close(t.end)
 })
 
@@ -19,7 +35,8 @@ test('Woolly - registering a handler is doable', t => {
   const server = app.listen(3000)
   let w = WoollyServer()
 
-  let result = w.handler('/a', () => 'A', {})
+  let result = w.handler(buildCountingHandler())
+
   t.equal(result, w, 'handler should be chainable')
 
   server.close(t.end)
@@ -30,18 +47,12 @@ test('Woolly - actions sent to server get processed', t => {
   const server = app.listen(3000)
 
   let w = WoollyServer(server)
-
-  let count = 0
-  let result = w.handler('/a', () => count, {
-    inc: () => ++count
-  })
+  w.handler(buildCountingHandler())
 
   let calls = 0
-  let client = WoollyClient('http://localhost:3000/a/', state => {
-    t.deepEqual(state, [0, 1][calls++], 'expected change content')
-    if (calls === 2) {
-      tearDown(client, server, t.end)
-    }
+  let client = WoollyClient('http://localhost:3000/count/', state => {
+    t.equal(state, [0, 1][calls++], 'expected change content')
+    if (calls === 2) tearDown(client, server, t.end)
   })
 
   client.do('inc')
@@ -54,17 +65,17 @@ test('Woolly - handlers pass params to their view and actions', t => {
   let w = WoollyServer(server)
 
   let client = WoollyClient('http://localhost:3000/foo/bar', state => {
-    t.deepEqual(state, { param1: 'foo', param2: 'bar' })
+    t.deepEqual(state, {param1: 'foo', param2: 'bar'})
   })
 
   let result = w.handler('/:param1/:param2', params => params, {
     check: params => {
-      t.deepEqual(params, { param1: 'foo', param2: 'bar', x: 10 })
+      t.deepEqual(params, {param1: 'foo', param2: 'bar', x: 10})
       tearDown(client, server, t.end)
     }
   })
 
-  client.do('check', { x: 10 })
+  client.do('check', {x: 10})
 })
 
 test('Woolly - active server can be connected to', t => {
@@ -73,10 +84,10 @@ test('Woolly - active server can be connected to', t => {
 
   let w = WoollyServer(server)
 
-  let result = w.handler('/a', () => 'A', {})
+  let result = w.handler('/a', () => ['A'], {})
 
   let client = WoollyClient('http://localhost:3000/a/', state => {
-    t.deepEqual(state, 'A', 'received correct state in change callback')
+    t.deepEqual(state, ['A'], 'received correct state in change callback')
     tearDown(client, server, t.end)
   })
 })
@@ -87,25 +98,59 @@ test('Woolly - handler can be configured using an object', t => {
 
   let w = WoollyServer(server)
 
+  let result = w.handler(buildCountingHandler())
+
+  let calls = 0
+  let client = WoollyClient('http://localhost:3000/count/', state => {
+    t.deepEqual(state, [0, 1][calls++], 'expected state content')
+    if (calls === 2) tearDown(client, server, t.end)
+  })
+
+  client.do('inc')
+})
+
+test('Woolly - actions can return values', t => {
+  const app = require('express')()
+  const server = app.listen(3000)
+
+  let w = WoollyServer(server)
+
+  let result = w.handler(buildCountingHandler())
+
+  let calls = 0
+  let client = WoollyClient('http://localhost:3000/count/', state => {
+    t.deepEqual(state, [0, 1][calls++], 'expected state content')
+    if (calls === 2) tearDown(client, server, t.end)
+  })
+
+  client.do('inc').then(count => {
+    t.equal(count, 1, 'response was available from action')
+  })
+})
+
+test('Woolly - actions can reject', t => {
+  const app = require('express')()
+  const server = app.listen(3000)
+
+  let w = WoollyServer(server)
+
   let count = 0
   let result = w.handler({
     route: '/test',
-    getState: () => count,
+    getState: () => null,
     actions: {
-      tick: () => ++count
+      tick: () => {
+        throw new Error('what!?')
+      }
     }
   })
 
   let calls = 0
-  let client = WoollyClient('http://localhost:3000/test/', state => {
-    t.deepEqual(state, [0, 1][calls++], 'expected state content')
-    if (calls === 2) t.end()
-  })
-  client.do('tick')
-})
+  let client = WoollyClient('http://localhost:3000/test/', state => {})
 
-function tearDown (client, server, cb) {
-  client.disconnect(
-    err => (err ? cb(err) : setTimeout(() => server.close(cb), 50))
-  )
-}
+  client.do('tick').catch(err => {
+    t.ok(err instanceof Error)
+    t.equal(err.message, 'what!?', 'message on error should be passed through')
+    tearDown(client, server, t.end)
+  })
+})
